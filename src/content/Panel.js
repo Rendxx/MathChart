92deed167f069a3b9b36f7d419e6a828ad367792
Rendxx <rendxx@gmail.com>
2017-01-26 16:12:33 -0600

    ad just style

--- src/content/Panel.js
+++ src/content/Panel.js
@@ -31,10 +31,10 @@ var Panel = React.createClass({
     return(
       <div className="__panelWrap" ref="self">
           <div className="__panelLeft" ref="left" style={{"width":this.state.left+"px"}}>
-              <Component_Input ref="minX" tag="Min X" left={80} width={280} value="0" customClassName="inputItem" color="#333" valueCheck={this._checkCorValue} onChanged={this._setChartPara.bind(this)}/>
-              <Component_Input ref="maxX" tag="Max X" left={80} width={280} value="10" customClassName="inputItem" color="#333" valueCheck={this._checkCorValue} onChanged={this._setChartPara.bind(this)}/>
-              <Component_Input ref="minY" tag="Min Y" left={80} width={280} value="0" customClassName="inputItem" color="#333" valueCheck={this._checkCorValue} onChanged={this._setChartPara.bind(this)}/>
-              <Component_Input ref="maxY" tag="Max Y" left={80} width={280} value="10" customClassName="inputItem" color="#333" valueCheck={this._checkCorValue} onChanged={this._setChartPara.bind(this)}/>
+              <Component_Input ref="minX" tag="Min X" left={80} width={260} value="0" customClassName="inputItem" color="#333" valueCheck={this._checkCorValue} onChanged={this._setChartPara.bind(this)}/>
+              <Component_Input ref="maxX" tag="Max X" left={80} width={260} value="10" customClassName="inputItem" color="#333" valueCheck={this._checkCorValue} onChanged={this._setChartPara.bind(this)}/>
+              <Component_Input ref="minY" tag="Min Y" left={80} width={260} value="0" customClassName="inputItem" color="#333" valueCheck={this._checkCorValue} onChanged={this._setChartPara.bind(this)}/>
+              <Component_Input ref="maxY" tag="Max Y" left={80} width={260} value="10" customClassName="inputItem" color="#333" valueCheck={this._checkCorValue} onChanged={this._setChartPara.bind(this)}/>
           </div>
           <div className="__panelRight" ref="right" style={{"margin-left":this.state.left+"px"}}>
             <MathChart/>
